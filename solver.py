94a206e3b4c717dd52b1a70685b55495f6b24d51
Трефилов Андрей <trefilov9@ad.is74.ru>
2020-01-23 15:42:26 +0500

    random for p >= 0.5

--- solver.py
+++ solver.py
@@ -4,6 +4,7 @@ from load import from_pgm
 from filtration import median_filter, r_0
 from line_detection import hough_line
 from find_vertices import find_vertices
+import random
 import sys
 
 
@@ -14,8 +15,10 @@ if __name__ == "__main__":
         raw_img = from_pgm(sys.argv[2])
         count_black = raw_img[raw_img == 0].shape[0] / 250000
 
-        if count_black < 0.68:
-            filter_size = 7
+        if count_black < 0.58:
+            filter_size = 'random'
+        elif count_black > 0.58 and count_black < 0.68:
+            filter_size = 9
         elif count_black > 0.68 and count_black < 0.78:
             filter_size = 5
         elif count_black > 0.78 and count_black < 0.98:
@@ -25,8 +28,10 @@ if __name__ == "__main__":
 
         if filter_size is None:
             y0, x0, y1, x1, y2, x2 = r_0(raw_img)
+        elif filter_size == 'random':
+            y0, x0, y1, x1, y2, x2 = tuple([random.uniform(0, 499) for _ in range(6)])
         else:
             accumulator, thetas, rhos = hough_line(median_filter(raw_img, filter_size))
             y0, x0, y1, x1, y2, x2 = find_vertices(accumulator, thetas, rhos)
         save_output(y0, x0, y1, x1, y2, x2)
-        print(y0, x0, y1, x1, y2, x2)
+        # print(y0, x0, y1, x1, y2, x2)
